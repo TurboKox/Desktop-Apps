d903ee49a3fe868199518a413086bddd9d840d8b
TurboKox <93782955+TurboKox@users.noreply.github.com>
2022-11-08 14:42:52 +0100

    Create MainWindow.xaml.cs

---
+++ Tic Tac Toe/MainWindow.xaml.cs
@@ -0,0 +1,173 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tic_Tac_Toe
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            buttons[0] = Btn0;
+            buttons[1] = Btn1;
+            buttons[2] = Btn2;
+            buttons[3] = Btn3;
+            buttons[4] = Btn4;
+            buttons[5] = Btn5;
+            buttons[6] = Btn6;
+            buttons[7] = Btn7;
+            buttons[8] = Btn8;
+        }
+
+        private void SwapTurns()
+        {
+            circleTurn = !circleTurn;
+            if (!gameOver)
+                TurnTextBlock.Text = circleTurn ? "Kolej: O | Ilosc ruchow: " + moves : "Kolej: X | Ilosc ruchow: " + moves;
+        }
+
+        private void PlaceMark(Button btn, string currentTurn)
+        {
+            if (!gameOver)
+            {
+                if (btn.Content == null)
+                {
+                    btn.Content = currentTurn;
+                }
+                else
+                {
+                    SwapTurns();
+                    moves--;
+                }
+            }
+        }
+
+        private void CheckWin(string currentTurn)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                if ((string)buttons[winningCombinations[i][0]].Content == currentTurn && 
+                    (string)buttons[winningCombinations[i][1]].Content == currentTurn &&
+                    (string)buttons[winningCombinations[i][2]].Content == currentTurn)
+                {
+                    gameOver = true;
+                    TurnTextBlock.Text = currentTurn + " wygral!";
+                }
+            }
+        }
+
+        private void CheckDraw()
+        {
+            if (moves == 9 && !gameOver)
+            {
+                gameOver = true;
+                TurnTextBlock.Text = "Remis!";
+            }
+        }
+
+        private void Restart()
+        {
+            moves = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                TurnTextBlock.Text = "Kolej: O | Ilosc ruchow: 0";
+                buttons[i].Content = null;
+                gameOver = false;
+                circleTurn = true;
+            }
+        }
+
+        private void FunctionInit(Button btn)
+        {
+            string currentTurn = circleTurn ? "O" : "X";
+            PlaceMark(btn, currentTurn);
+            CheckWin(currentTurn);
+            moves++;
+            CheckDraw();
+            SwapTurns();
+        }
+
+        private void Btn0Click(object sender, RoutedEventArgs e)
+        {
+            FunctionInit(Btn0);
+        }
+
+        private void Btn1Click(object sender, RoutedEventArgs e)
+        {
+            FunctionInit(Btn1);
+        }
+
+        private void Btn2Click(object sender, RoutedEventArgs e)
+        {
+            FunctionInit(Btn2);
+        }
+
+        private void Btn3Click(object sender, RoutedEventArgs e)
+        {
+            FunctionInit(Btn3);
+        }
+
+        private void Btn4Click(object sender, RoutedEventArgs e)
+        {
+            FunctionInit(Btn4);
+        }
+
+        private void Btn5Click(object sender, RoutedEventArgs e)
+        {
+            FunctionInit(Btn5);
+        }
+
+        private void Btn6Click(object sender, RoutedEventArgs e)
+        {
+            FunctionInit(Btn6);
+        }
+
+        private void Btn7Click(object sender, RoutedEventArgs e)
+        {
+            
+            FunctionInit(Btn7);
+        }
+
+        private void Btn8Click(object sender, RoutedEventArgs e)
+        {
+            
+            FunctionInit(Btn8);
+        }
+        private void BtnRestartClick(object sender, RoutedEventArgs e)
+        {
+            Restart();
+        }
+        
+        private Button[] buttons = new Button[9];
+
+        private bool circleTurn = true;
+        private int moves = 0;
+        private bool gameOver = false;
+
+        private int[][] winningCombinations = {
+                new int[] { 0, 1, 2 },
+                new int[] { 3, 4, 5 },
+                new int[] { 6, 7, 8 },
+                new int[] { 0, 3, 6 },
+                new int[] { 1, 4, 7 },
+                new int[] { 2, 5, 8 },
+                new int[] { 0, 4, 8 },
+                new int[] { 2, 4, 6 }
+        };
+    }
+}
